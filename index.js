1ff7b54247d8cdf72b5e9a0325e3f4b0d921c5b5
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-06-11 00:24:08 +0600

    added role changes for admin and instructor

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ async function run() {
     });
 
     // users related apis
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyJWT, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -252,12 +252,24 @@ async function run() {
       res.send(result);
     });
 
-    app.patch("/user/admin/:id", async (req, res) => {
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
           role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "instructor",
         },
       };
       const result = await usersCollection.updateOne(filter, updatedDoc);
