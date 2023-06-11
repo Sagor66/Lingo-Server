1b90f42f96655a051100439adf529bab3654ef1a
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-06-11 13:43:08 +0600

    added feedback api

--- index.js
+++ index.js
@@ -276,6 +276,18 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/new-classes/feedback/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          feedback: "instructor",
+        },
+      };
+      const result = await newClassesCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
